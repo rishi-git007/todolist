c7755abdfef6c2888206b75441e4d59d472ea082
RISHI VARUN MACHA <rishi_vm@ee.iitr.ac.in>
2024-12-30 19:10:52 +0530

    Add files via upload

---
+++ ToDoList.js
@@ -0,0 +1,34 @@
+const todolist = [];
+
+function addTask(){
+    const dueDate = document.querySelector(
+        '.due-date-input');
+    const TaskElem = document.querySelector('.js-task');
+    const date = dueDate.value;
+    const task = TaskElem.value;
+    const taskObj = {
+        date ,
+        task
+    }
+    if(task !='' && date !=''){
+    todolist.push( taskObj);
+    TaskElem.value='';
+    }
+
+}
+function displayTasks(){
+    const ListElem = document.querySelector('.js-tasklist');
+    ListElem.innerHTML = '';
+    for(let i = 0 ; i < todolist.length ; i++ )
+    { 
+        const date = todolist[i].date;
+        const task = todolist[i].task;
+        ListElem.innerHTML += `<p class="tasks"
+        > ${task}  ${date} 
+        <button class="delete-task" 
+        onclick=" todolist.splice(${ i }, 1);
+        displayTasks();"
+        >Delete</button> </p>`;
+    } 
+}
+
